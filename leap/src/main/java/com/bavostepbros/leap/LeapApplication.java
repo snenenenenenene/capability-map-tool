15a570881b9d1a33692444542417501b866be7cc
oatmeal <snenene@pop-os.localdomain>
2021-04-19 15:33:08 +0200

    first commit

---
+++ leap/src/main/java/com/bavostepbros/leap/LeapApplication.java
@@ -0,0 +1,13 @@
+package com.bavostepbros.leap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeapApplication.class, args);
+    }
+
+}
---
+++ leap/src/test/java/com/bavostepbros/leap/LeapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bavostepbros.leap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
