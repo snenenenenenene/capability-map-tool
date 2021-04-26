4d8a049c5a03c60cb27f901484ec67c573319b12
snenene <sennebels@gmail.com>
2021-04-26 09:51:06 +0200

    second commit

---
+++ leap/src/main/java/com/bavostepbros/leap/controller/LeapController.java
@@ -0,0 +1,24 @@
+package com.bavostepbros.leap.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LeapController {
+    @GetMapping("/")
+    public String index(){
+        return  "redirect:/login";
+    }
+
+
+    @GetMapping("/authorize")
+    public String AuthorizationPage(){
+        return "authorize";
+    }
+}
