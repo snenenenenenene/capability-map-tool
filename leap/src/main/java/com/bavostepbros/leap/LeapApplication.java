798cecd0dfdb6d82360000446d4bf18c6ec57fa7
snenene <sennebels@gmail.com>
2021-04-26 09:21:40 +0200

    pom.xml update

--- leap/src/main/java/com/bavostepbros/leap/LeapApplication.java
+++ leap/src/main/java/com/bavostepbros/leap/LeapApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeapApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LeapApplication.class, args);
     }
-
 }
