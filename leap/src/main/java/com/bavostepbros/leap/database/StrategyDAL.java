1f573bebb0619743d778b3895a69c5d2464cfb08
cheche <s113723@ap.be>
2021-04-29 16:34:05 +0200

    added strategies

--- leap/src/main/java/com/bavostepbros/leap/database/StrategyDAL.java
+++ leap/src/main/java/com/bavostepbros/leap/database/StrategyDAL.java
@@ -3,7 +3,6 @@ package com.bavostepbros.leap.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.bavostepbros.leap.model.Strategy;
 
-public interfaceStrategyDAL extends JpaRepository<Strategy, Integer> {
-    
+public interface StrategyDAL extends JpaRepository<Strategy, Integer> {
 
 }
