707c16cd268cdd2b87e3736451a569c88f4df1ed
cheche <s113723@ap.be>
2021-05-03 14:40:39 +0200

    added docs

--- leap/src/main/java/com/bavostepbros/leap/controller/EnvironmentController.java
+++ leap/src/main/java/com/bavostepbros/leap/controller/EnvironmentController.java
@@ -5,15 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.bavostepbros.leap.domain.customexceptions.DuplicateValueException;
@@ -29,9 +23,9 @@ public class EnvironmentController {
 	@Autowired
 	private EnvironmentService envService;
 	
-	@PostMapping(path = "/environment/add", consumes = "application/json")
+	@PostMapping(path = "/environment/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<Void> addEnvironment(
-			@RequestBody Environment environment, 
+			@ModelAttribute Environment environment,
 			UriComponentsBuilder builder) {
 		if (environment.getEnvironmentName() == null || 
 				environment.getEnvironmentName().isBlank() || 
