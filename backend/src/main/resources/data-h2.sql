7440710ca4e9bce1e52d1563ffd1c3c9fa4b3e7f
snenene <sennebels@gmail.com>
2021-05-21 15:31:07 +0200

    Selenium Test + Auth & Home Refactor

--- backend/src/main/resources/data-h2.sql
+++ backend/src/main/resources/data-h2.sql
@@ -3,5 +3,5 @@ INSERT INTO ROLE (ROLEID, ROLENAME) VALUES
 (2, 'Admin');
 
 INSERT INTO USER (USERID, ROLEID, USERNAME, PASSWORD, EMAIL) VALUES
-(1, 1, 'test_user', 'test_user', 'test_user'),
-(2, 2, 'super_admin', 'super_admin', 'super_admin');
+(1, 1, 'test_user', '31676025316fb555e0bfa12f0bcfd0ea43c4c20e', 'test_user'),
+(2, 2, 'super_admin', '736a95382da0c1b931dc87529706d25c636954e7', 'super_admin');
