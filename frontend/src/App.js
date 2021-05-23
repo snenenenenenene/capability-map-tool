fb91584589ad6b61bc00ff26498a98b40b0c5717
senne <50323795+snenne@users.noreply.github.com>
2021-05-23 19:48:03 +0200

    im getting mad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -157,7 +157,7 @@ class App extends Component {
               <Switch>
                 {/* ROOT */}
                 <Route exact path='/home' component={Home} />
-                <Route exact path='/' component={Home} />
+                {/* <Route exact path='/' component={Home} /> */}
                 {/* ENVIRONMENTS */}
                 <Route exact path='/add' component={NewEnvironment} />
                 <Route
