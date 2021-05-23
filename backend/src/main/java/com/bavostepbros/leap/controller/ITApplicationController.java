11824bae53c06dd75bc3b8e16f76bafaeb2b1654
BavoVanMeel <bavo1997@gmail.com>
2021-05-24 01:21:56 +0200

    Fixed ITApplicationController

--- backend/src/main/java/com/bavostepbros/leap/controller/ITApplicationController.java
+++ backend/src/main/java/com/bavostepbros/leap/controller/ITApplicationController.java
@@ -1,113 +1,163 @@
 package com.bavostepbros.leap.controller;
 
-import com.bavostepbros.leap.domain.customexceptions.DuplicateValueException;
-import com.bavostepbros.leap.domain.customexceptions.IndexDoesNotExistException;
-import com.bavostepbros.leap.domain.customexceptions.InvalidInputException;
 import com.bavostepbros.leap.domain.model.ITApplication;
+import com.bavostepbros.leap.domain.model.dto.ITApplicationDto;
 import com.bavostepbros.leap.domain.service.itapplicationservice.ITApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/itapplication")
 public class ITApplicationController {
 
-    @Autowired
-    private ITApplicationService itApplicationService;
+	@Autowired
+	private ITApplicationService itApplicationService;
 
-    @PostMapping(path = "/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<Void> addITApplication(
-            @ModelAttribute("statusId") Integer statusID,
-            @ModelAttribute("name") String name,
-            @ModelAttribute("version") String version,
-            @ModelAttribute("purchaseDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
-            @ModelAttribute("endOfLife") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endOfLife,
-            @ModelAttribute("currentScalability") Integer currentScalability,
-            @ModelAttribute("expectedScalability") Integer expectedScalability,
-            @ModelAttribute("currentPerformance") Integer currentPerformance,
-            @ModelAttribute("expectedPerformance") Integer expectedPerformance,
-            @ModelAttribute("currentSecurityLevel") Integer currentSecurityLevel,
-            @ModelAttribute("expectedSecurityLevel") Integer expectedSecurityLevel,
-            @ModelAttribute("currentStability") Integer currentsStability,
-            @ModelAttribute("expectedStability") Integer expectedStability,
-            @ModelAttribute("costCurrency") String costCurrency,
-            @ModelAttribute("currentValue") String currentValue,
-            @ModelAttribute("currentYearlyCost") Double currentYearlyCost,
-            @ModelAttribute("timeValue") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate timeValue,
-            UriComponentsBuilder builder) {
-        if(itApplicationService.existsByName(name)) throw new DuplicateValueException("An application already exists with this name.");
-        long applicationId = itApplicationService.save(statusID, name, version, purchaseDate, endOfLife,
-                currentScalability, expectedScalability, currentPerformance, expectedPerformance, currentSecurityLevel,
-                expectedSecurityLevel, currentsStability, expectedStability, costCurrency, currentValue,
-                currentYearlyCost, timeValue).getItApplicationId();
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ITApplicationDto addITApplication(
+			@ModelAttribute("statusId") Integer statusID,
+			@ModelAttribute("name") String name, 
+			@ModelAttribute("version") String version,
+			@ModelAttribute("purchaseDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
+			@ModelAttribute("endOfLife") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endOfLife,
+			@ModelAttribute("currentScalability") Integer currentScalability,
+			@ModelAttribute("expectedScalability") Integer expectedScalability,
+			@ModelAttribute("currentPerformance") Integer currentPerformance,
+			@ModelAttribute("expectedPerformance") Integer expectedPerformance,
+			@ModelAttribute("currentSecurityLevel") Integer currentSecurityLevel,
+			@ModelAttribute("expectedSecurityLevel") Integer expectedSecurityLevel,
+			@ModelAttribute("currentStability") Integer currentsStability,
+			@ModelAttribute("expectedStability") Integer expectedStability,
+			@ModelAttribute("costCurrency") String costCurrency, 
+			@ModelAttribute("currentValue") String currentValue,
+			@ModelAttribute("currentYearlyCost") Double currentYearlyCost,
+			@ModelAttribute("timeValue") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate timeValue) {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(builder.path("/get/{id}").buildAndExpand(applicationId).toUri());
-        return new ResponseEntity<>(headers, HttpStatus.CREATED);
-    }
+		ITApplication itApplication = itApplicationService.save(statusID, name, version, purchaseDate, endOfLife,
+				currentScalability, expectedScalability, currentPerformance, expectedPerformance, currentSecurityLevel,
+				expectedSecurityLevel, currentsStability, expectedStability, costCurrency, currentValue,
+				currentYearlyCost, timeValue);
+		return new ITApplicationDto(itApplication.getItApplicationId(), itApplication.getStatus(),
+				itApplication.getName(), itApplication.getVersion(), itApplication.getPurchaseDate(),
+				itApplication.getEndOfLife(), itApplication.getCurrentScalability(),
+				itApplication.getExpectedScalability(), itApplication.getCurrentPerformance(),
+				itApplication.getExpectedPerformance(), itApplication.getCurrentSecurityLevel(),
+				itApplication.getExpectedSecurityLevel(), itApplication.getCurrentStability(),
+				itApplication.getExpectedScalability(), itApplication.getCostCurrency(),
+				itApplication.getCurrentValue(), itApplication.getCurrentYearlyCost(), itApplication.getTimeValue(),
+				itApplication.getTechnologies());
+	}
 
-    @GetMapping(path = "/get/{id}")
-    public ResponseEntity<ITApplication> getITApplication(@RequestParam Integer id){
-        if(!itApplicationService.existsById(id)) throw new IndexDoesNotExistException("Application not found.");
-        return new ResponseEntity<ITApplication>(itApplicationService.get(id), HttpStatus.OK);
-    }
+	@GetMapping(path = "{itApplicationId}")
+	public ITApplicationDto getITApplicationById(@PathVariable("itApplicationId") Integer itApplicationId) {
+		ITApplication itApplication = itApplicationService.get(itApplicationId);
+		return new ITApplicationDto(itApplication.getItApplicationId(), itApplication.getStatus(),
+				itApplication.getName(), itApplication.getVersion(), itApplication.getPurchaseDate(),
+				itApplication.getEndOfLife(), itApplication.getCurrentScalability(),
+				itApplication.getExpectedScalability(), itApplication.getCurrentPerformance(),
+				itApplication.getExpectedPerformance(), itApplication.getCurrentSecurityLevel(),
+				itApplication.getExpectedSecurityLevel(), itApplication.getCurrentStability(),
+				itApplication.getExpectedScalability(), itApplication.getCostCurrency(),
+				itApplication.getCurrentValue(), itApplication.getCurrentYearlyCost(), itApplication.getTimeValue(),
+				itApplication.getTechnologies());
+	}
+	
+	@PutMapping(path = "{itApplicationId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ITApplicationDto updateITApplication(@PathVariable("itApplicationId") Integer itApplicationId,
+			@ModelAttribute("statusId") Integer statusID, @ModelAttribute("name") String name,
+			@ModelAttribute("version") String version,
+			@ModelAttribute("purchaseDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
+			@ModelAttribute("endOfLife") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endOfLife,
+			@ModelAttribute("currentScalability") Integer currentScalability,
+			@ModelAttribute("expectedScalability") Integer expectedScalability,
+			@ModelAttribute("currentPerformance") Integer currentPerformance,
+			@ModelAttribute("expectedPerformance") Integer expectedPerformance,
+			@ModelAttribute("currentSecurityLevel") Integer currentSecurityLevel,
+			@ModelAttribute("expectedSecurityLevel") Integer expectedSecurityLevel,
+			@ModelAttribute("currentStability") Integer currentsStability,
+			@ModelAttribute("expectedStability") Integer expectedStability,
+			@ModelAttribute("costCurrency") String costCurrency, @ModelAttribute("currentValue") String currentValue,
+			@ModelAttribute("currentYearlyCost") Double currentYearlyCost,
+			@ModelAttribute("timeValue") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate timeValue) {
+		
+		ITApplication itApplication = itApplicationService.update(itApplicationId, statusID, name, version, purchaseDate, endOfLife,
+				currentScalability, expectedScalability, currentPerformance, expectedPerformance, currentSecurityLevel,
+				expectedSecurityLevel, currentsStability, expectedStability, costCurrency, currentValue,
+				currentYearlyCost, timeValue);
+		return new ITApplicationDto(itApplication.getItApplicationId(), itApplication.getStatus(),
+				itApplication.getName(), itApplication.getVersion(), itApplication.getPurchaseDate(),
+				itApplication.getEndOfLife(), itApplication.getCurrentScalability(),
+				itApplication.getExpectedScalability(), itApplication.getCurrentPerformance(),
+				itApplication.getExpectedPerformance(), itApplication.getCurrentSecurityLevel(),
+				itApplication.getExpectedSecurityLevel(), itApplication.getCurrentStability(),
+				itApplication.getExpectedScalability(), itApplication.getCostCurrency(),
+				itApplication.getCurrentValue(), itApplication.getCurrentYearlyCost(), itApplication.getTimeValue(),
+				itApplication.getTechnologies());
+	}
+	
+	@DeleteMapping(path = "{itApplicationId}")
+	public void deleteItApplication(@PathVariable("itApplicationId") Integer itApplicationId) {
+		itApplicationService.delete(itApplicationId);
+	}
+	
+	@GetMapping(path = "exists-by-id/{itApplicationId}")
+	public boolean doesItApplicationExistsById(@PathVariable("itApplicationId") Integer itApplicationId) {
+		return itApplicationService.existsById(itApplicationId);
+	}
+	
+	@GetMapping(path = "exists-by-name/{itApplicationName}")
+	public boolean doesItApplicationExistsById(@PathVariable("itApplicationName") String itApplicationName) {
+		return itApplicationService.existsByName(itApplicationName);
+	}
+	
+	@GetMapping(path = "{itApplicationName}")
+	public ITApplicationDto getITApplicationByName(@PathVariable("itApplicationName") String itApplicationName) {
+		ITApplication itApplication = itApplicationService.getItApplicationByName(itApplicationName);
+		return new ITApplicationDto(itApplication.getItApplicationId(), itApplication.getStatus(),
+				itApplication.getName(), itApplication.getVersion(), itApplication.getPurchaseDate(),
+				itApplication.getEndOfLife(), itApplication.getCurrentScalability(),
+				itApplication.getExpectedScalability(), itApplication.getCurrentPerformance(),
+				itApplication.getExpectedPerformance(), itApplication.getCurrentSecurityLevel(),
+				itApplication.getExpectedSecurityLevel(), itApplication.getCurrentStability(),
+				itApplication.getExpectedScalability(), itApplication.getCostCurrency(),
+				itApplication.getCurrentValue(), itApplication.getCurrentYearlyCost(), itApplication.getTimeValue(),
+				itApplication.getTechnologies());
+	}
 
-	/*
-	 * @GetMapping(path = "/{name}") public ResponseEntity<List<ITApplication>>
-	 * getITApplicationByName(@RequestParam String name){ return new
-	 * ResponseEntity<List<ITApplication>>(itApplicationService.get(name),
-	 * HttpStatus.OK); }
-	 */
-
-    @GetMapping(path = "/all")
-    public ResponseEntity<List<ITApplication>> getAllITApplications(){
-        return new ResponseEntity<>(itApplicationService.getAll(), HttpStatus.OK);
-    }
-
-    @PutMapping(path = "/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity updateITApplication(
-            @ModelAttribute("id") Integer id,
-            @ModelAttribute("statusId") Integer statusID,
-            @ModelAttribute("name") String name,
-            @ModelAttribute("version") String version,
-            @ModelAttribute("purchaseDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate purchaseDate,
-            @ModelAttribute("endOfLife") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endOfLife,
-            @ModelAttribute("currentScalability") Integer currentScalability,
-            @ModelAttribute("expectedScalability") Integer expectedScalability,
-            @ModelAttribute("currentPerformance") Integer currentPerformance,
-            @ModelAttribute("expectedPerformance") Integer expectedPerformance,
-            @ModelAttribute("currentSecurityLevel") Integer currentSecurityLevel,
-            @ModelAttribute("expectedSecurityLevel") Integer expectedSecurityLevel,
-            @ModelAttribute("currentStability") Integer currentsStability,
-            @ModelAttribute("expectedStability") Integer expectedStability,
-            @ModelAttribute("costCurrency") String costCurrency,
-            @ModelAttribute("currentValue") String currentValue,
-            @ModelAttribute("currentYearlyCost") Double currentYearlyCost,
-            @ModelAttribute("timeValue") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate timeValue) {
-        ITApplication updatedITApplication = itApplicationService.update(id, statusID, name, version, purchaseDate, endOfLife,
-                currentScalability, expectedScalability, currentPerformance, expectedPerformance, currentSecurityLevel,
-                expectedSecurityLevel, currentsStability, expectedStability, costCurrency,
-                currentValue, currentYearlyCost, timeValue);
-
-        return new ResponseEntity<>(updatedITApplication, HttpStatus.OK);
-    }
-
-    @DeleteMapping(path = "/delete/{id}")
-    public ResponseEntity<Void> deleteITApplication(@RequestParam Integer id){
-        if(id == null || id.equals(0)) throw new InvalidInputException("Invalid input exception");
-        if (!itApplicationService.existsById(id)) throw new IndexDoesNotExistException("ID does not exist.");
-        itApplicationService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+	@GetMapping
+	public List<ITApplicationDto> getAllITApplications() {
+		List<ITApplication> itApplications = itApplicationService.getAll();
+		List<ITApplicationDto> itApplicationsDto = itApplications.stream()
+				.map(itApplication -> new ITApplicationDto(itApplication.getItApplicationId(), itApplication.getStatus(),
+						itApplication.getName(), itApplication.getVersion(), itApplication.getPurchaseDate(),
+						itApplication.getEndOfLife(), itApplication.getCurrentScalability(),
+						itApplication.getExpectedScalability(), itApplication.getCurrentPerformance(),
+						itApplication.getExpectedPerformance(), itApplication.getCurrentSecurityLevel(),
+						itApplication.getExpectedSecurityLevel(), itApplication.getCurrentStability(),
+						itApplication.getExpectedScalability(), itApplication.getCostCurrency(),
+						itApplication.getCurrentValue(), itApplication.getCurrentYearlyCost(), itApplication.getTimeValue(),
+						itApplication.getTechnologies()))
+				.collect(Collectors.toList());
+		return itApplicationsDto;
+	}
+	
+	@GetMapping(path = "all-currencies")
+	public List<String> getAllCurrencies() {
+		return itApplicationService.getAllCurrencies();
+	}
 }
