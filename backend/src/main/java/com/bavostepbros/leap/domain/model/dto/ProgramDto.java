1aa9643b16b4a1504badc59483de0bbf9a52fe0e
BavoVanMeel <bavo1997@gmail.com>
2021-05-27 11:10:38 +0200

    Program
    - Added Program with constraints
    - Added ProgramDto

---
+++ backend/src/main/java/com/bavostepbros/leap/domain/model/dto/ProgramDto.java
@@ -0,0 +1,15 @@
+package com.bavostepbros.leap.domain.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgramDto {
+	private Integer programId;
+	private String programName;
+}
