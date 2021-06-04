62c4b9a56f3f4eed97ee2609b1d800e0fc4e7255
snenene <sennebels@gmail.com>
2021-06-04 15:59:55 +0200

    CORS Fix

--- backend/src/main/java/com/bavostepbros/leap/configuration/SimpleCORSFilter.java
+++ backend/src/main/java/com/bavostepbros/leap/configuration/SimpleCORSFilter.java
@@ -32,7 +32,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
 
         chain.doFilter(req, res);
     }
