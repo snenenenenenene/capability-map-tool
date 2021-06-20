05ec47e3e418f199654173b060dc0f6a8f8fbf9d
BavoVanMeel <bavo1997@gmail.com>
2021-06-20 02:44:39 +0200

    InformationServiceTest completed
    InformationServiceTest has now 100% code coverage!

--- backend/src/test/java/com/bavostepbros/leap/unittest/InformationServiceTest.java
+++ backend/src/test/java/com/bavostepbros/leap/unittest/InformationServiceTest.java
@@ -2,18 +2,21 @@ package com.bavostepbros.leap.unittest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.bavostepbros.leap.domain.model.BusinessProcess;
 import com.bavostepbros.leap.domain.model.Information;
 import com.bavostepbros.leap.domain.service.informationservice.InformationService;
 import com.bavostepbros.leap.persistence.InformationDAL;
@@ -60,6 +63,89 @@ public class InformationServiceTest {
 		assertNotNull(informationId);	
 		assertNotNull(informationName);
 		assertNotNull(informationDescription);
+	}
+	
+	@Test 
+	void should_returnInformation_whenSaveInformation() {
+		BDDMockito.given(informationDAL.save(BDDMockito.any(Information.class))).willReturn(informationFirst);
+		
+		Information information = informationService.save(informationName, informationDescription);
+		
+		assertNotNull(information);
+		assertTrue(information instanceof Information);
+		testInformation(informationFirst, information);
+	}
+	
+	@Test 
+	void should_throwNullPointerException_whenGetInformationById() {
+		String expectedErrorMessage = "Information does not exist.";
+		
+		Exception exception = assertThrows(NullPointerException.class, 
+				() -> informationService.get(informationId));
+		
+		assertEquals(expectedErrorMessage, exception.getMessage());
+	}
+	
+	@Test 
+	void should_returnInformation_whenGetInformationById() {
+		BDDMockito.given(informationDAL.findById(BDDMockito.anyInt())).willReturn(optionalInformationFirst);
+		
+		Information information = informationService.get(informationId);
+		
+		assertNotNull(information);
+		assertTrue(information instanceof Information);
+		testInformation(informationFirst, information);
+	}
+	
+	@Test 
+	void should_returnInformation_whenUpdateInformation() {
+		BDDMockito.given(informationDAL.save(BDDMockito.any(Information.class))).willReturn(informationFirst);
+		
+		Information information = informationService.update(informationId, informationName, informationDescription);
+		
+		assertNotNull(information);
+		assertTrue(information instanceof Information);
+		testInformation(informationFirst, information);
+	}
+	
+	@Test 
+	void should_verify_whenDeleteInformation() {
+		informationService.delete(informationId);
+		
+		Mockito.verify(informationDAL, Mockito.times(1)).deleteById(Mockito.eq(informationId));
+	}
+	
+	@Test 
+	void should_throwNullPointerException_whenGetInformationByName() {
+		String expectedErrorMessage = "Information does not exist.";
+		
+		Exception exception = assertThrows(NullPointerException.class, 
+				() -> informationService.getInformationByName(informationName));
+		
+		assertEquals(expectedErrorMessage, exception.getMessage());
+	}
+	
+	@Test 
+	void should_returnInformation_whenGetInformationByName() {
+		BDDMockito.given(informationDAL.findByInformationName(BDDMockito.anyString())).willReturn(optionalInformationFirst);
+		
+		Information information = informationService.getInformationByName(informationName);
+		
+		assertNotNull(information);
+		assertTrue(information instanceof Information);
+		testInformation(informationFirst, information);
+	}
+	
+	@Test 
+	void should_returnInformations_whenGetAllInformation() {
+		BDDMockito.given(informationDAL.findAll()).willReturn(informations);
+		
+		List<Information> informationsResult = informationService.getAll();
+		
+		assertNotNull(informationsResult);
+		assertEquals(informations.size(), informationsResult.size());
+		testInformation(informationFirst, informationsResult.get(0));
+		testInformation(informationSecond, informationsResult.get(1));
 	}
 	
 	private void testInformation(Information expectedObject, Information actualObject) {
