351d12c7585cfb0fececa53cd8ac5243d77db202
WannesClaeys <64898522+WannesClaeys@users.noreply.github.com>
2021-06-21 23:39:18 +0200

    Capabiltiy JavaDocs

--- backend/src/main/java/com/bavostepbros/leap/domain/service/emailservice/EmailService.java
+++ backend/src/main/java/com/bavostepbros/leap/domain/service/emailservice/EmailService.java
@@ -13,14 +13,4 @@ public interface EmailService {
     void sendNewUserMessage(String to, String password);
 
     void sendForgotPassword(String to, String password);
-
-/*    void sendSimpleMessageUsingTemplate(String to, String subject, String ...templateModel);
-
-    void sendMessageWithAttachment(String to, String subject, String text, String pathToAttachment);
-    
-    void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel) 
-            throws IOException, MessagingException;
-
-    void sendMessageUsingFreemarkerTemplate(String to, String subject, Map<String, Object> templateModel)
-            throws IOException, TemplateException, MessagingException; */
 }
